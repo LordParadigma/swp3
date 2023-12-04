a78314ce37d859ff3fea72521aed8a1bf90afb89
Paradigma <fs@>
2023-12-04 17:46:16 +0100

    Rechner abgeschlossen

--- src/at/is/Grundlagen/oop/Rechner/WissenschaftlicherRechner.java
+++ src/at/is/Grundlagen/oop/Rechner/WissenschaftlicherRechner.java
@@ -1,17 +1,15 @@
 package at.is.Grundlagen.oop.Rechner;
 
-
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
+import static java.lang.Math.*;
 
 public class WissenschaftlicherRechner extends Rechner{
     public void Sinus(double Zahl){
-        double Ergebniss = sin(Zahl);
+        double Ergebniss = sin(toRadians(Zahl));
         System.out.println("Das Ergebniss beträgt " + Ergebniss);
     }
 
     public void Cosinus(double Zahl){
-        double Ergebniss = cos(Zahl);
+        double Ergebniss = cos(toRadians(Zahl));
         System.out.println("Das Ergebniss beträgt " + Ergebniss);
     }
 }
